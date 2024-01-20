a045bcded837cb2fab41350c423309982338668e
Armando Bugarin <armando.bugarin24@gmail.com>
2024-01-19 22:55:11 -0800

    edit game.py

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,4 +1,5 @@
 from ten_thousand.game_logic import GameLogic
+import sys
 
 # need to import GameLogic from game_logic.py?
 # dice_roller = GameLogic.roll_dice -- need for lab 8?
@@ -11,15 +12,21 @@ current_round = 1
 total_score = 0
 current_round = 1
 current_score = 0
+num_rounds = 5
 dice_to_set_aside = set()
 
-def play(roller=None, num_rounds = 5): # changed from main to play to match test_sim_basic.py
+def play(roller=None): # changed from main to play to match test_sim_basic.py
+
+    invite_to_play()
+
+    start_game()
 
     while True:
         start_round(num_rounds) # has argument; real deal variable
 
-        num_dice = int(input("Enter the number of dice to roll: "))
-        dice_values = roll_dice(num_dice)
+        # num_dice = input("Enter the number of dice to roll:")
+        # print(num_dice)
+        # dice_values = roll_dice(num_dice)
 
         choice = input("Do you want to set aside dice? (y/n): ").lower()
         if choice == 'y':
@@ -35,11 +42,36 @@ def play(roller=None, num_rounds = 5): # changed from main to play to match test
 def invite_to_play():
      print("Welcome to Ten Thousand")
      print("(y)es to play or (n)o to decline")
-     choice = input().lower()
-     return choice == 'y'
+    #  play()
+     choice = input("> ").lower()
+     if choice == 'n':
+         print("OK. Maybe another time")
+         sys.exit() # short for system, will stop file
+        #  exit()
+     elif choice == 'y':
+         return True
+     else:
+         print("Invalid choice. Please enter 'y' or 'n'.")
+         return invite_to_play()
+    #  play()
+     
+
+def start_game():
+     global num_rounds
+     print("Starting round 1")
+     print("Rolling 6 dice...")
+
+     num_dice = 6
+     dice_values = roll_dice(num_dice)
+     
+     print("Enter dice to keep, or (q)uit:")
+     choice = input("> ").lower()
+     if choice == 'q':
+          print("Thanks for playing. You earned 0 points")
+          sys.exit()
 
 
-def start_round(number_rounds): # paramter name should never be same as real variable name. but it can be similar
+def start_round(number_rounds): # parameter name should never be same as real variable name. but it can be similar
         if current_round < number_rounds:
             print(f"Starting Round {current_round}")
             dice_to_set_aside.clear()
@@ -66,11 +98,15 @@ def dice_to_roll(dice_values, dice_to_set_aside):
     return dice_to_roll
      
 
-
 # determine the num_dice for the next roll. dice_to_roll = len(dice_values-)- len(dice_to_set_aside).
 def set_aside(dice_values):
         dice_to_set_aside.update(dice_values)
         print(f"Setting aside dice: {dice_to_set_aside}")
+
+
+def num_dice_to_roll(dice_values, dice_to_set_aside):
+    num_dice_to_roll = len(dice_values) - len(dice_to_set_aside)
+    return num_dice_to_roll
 
 
 def calculate_score_and_bank():
@@ -105,4 +141,4 @@ def next_round_or_quit():
 
 if __name__ == "__main__":
     invite_to_play()
-    play()
+    # play()
